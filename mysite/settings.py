59befb28f6a2f0633b400f1692a5ecbf14a8418a
Hajajurgola <pkag@gmx.de>
2022-03-15 19:11:17 +0100

    bootstrap

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap5',
     'blog.apps.BlogConfig',
 ]
 
@@ -125,3 +126,5 @@ STATIC_ROOT = BASE_DIR / 'static'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
